853cb984e74e30d8af9d51e477b27e2699e1d479
Amir Guindehi <amir.guindehi@mgb.ch>
2025-09-07 00:51:17 +0200

    refactor: simplify fork merge instruction output

--- src/main.rs
+++ src/main.rs
@@ -2441,40 +2441,17 @@ fn fork_run(cli: &Cli, panes: usize) -> ExitCode {
                     sid
                 );
                 println!("To inspect and merge changes, you can run:");
-                let wrapper = if cfg!(target_os = "windows") { "aifo-coder" } else { "./aifo-coder" };
-                println!("  {} fork merge --session {} --strategy fetch", wrapper, sid);
-                println!("  {} fork merge --session {} --strategy octopus --autoclean", wrapper, sid);
                 if let Some((first_dir, first_branch)) = clones.first() {
                     println!("  git -C \"{}\" status", first_dir.display());
                     println!(
                         "  git -C \"{}\" log --oneline --decorate --graph -n 20",
                         first_dir.display()
                     );
-                    println!(
-                        "  git -C \"{}\" remote add fork-{}-1 \"{}\"  # once",
-                        repo_root.display(),
-                        sid,
-                        first_dir.display()
-                    );
-                    println!(
-                        "  git -C \"{}\" fetch fork-{}-1 {}",
-                        repo_root.display(),
-                        sid,
-                        first_branch
-                    );
-                    if base_label != "detached" {
-                        println!(
-                            "  git -C \"{}\" checkout {}",
-                            repo_root.display(),
-                            base_ref_or_sha
-                        );
-                        println!(
-                            "  git -C \"{}\" merge --no-ff {}",
-                            repo_root.display(),
-                            first_branch
-                        );
-                    }
                 }
+                println!();
+                let wrapper = if cfg!(target_os = "windows") { "aifo-coder" } else { "./aifo-coder" };
+                println!("  {} fork merge --session {} --strategy fetch", wrapper, sid);
+                println!("  {} fork merge --session {} --strategy octopus --autoclean", wrapper, sid);
                 return ExitCode::from(0);
             }
         }
