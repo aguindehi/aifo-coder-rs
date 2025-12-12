b5cab67c8cb8ec710c4bbeea55522e484383fcd2
Amir Guindehi <amir.guindehi@mgb.ch>
2025-12-12 19:55:02 +0100

    chore: remove deprecated util/docker module

--- src/util/docker.rs
+++
@@ -1,5 +0,0 @@
-#![allow(dead_code)]
-// Deprecated: prefer `aifo_coder::docker::image_exists` (re-exported from docker_mod).
-//
-// Kept temporarily as an empty compatibility module so older codepaths don't break if they
-// still reference `util::docker`, but should be removed once confirmed unused.
